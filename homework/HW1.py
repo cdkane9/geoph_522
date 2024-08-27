7b1d08c92b84e2ddd6d4c4fa7dba60459d5d54e3
cdkane9 <colemankane@boisestate.edu>
2024-08-27 11:26:10 -0600

    setting up repo

---
+++ homework/HW1.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy import stats as st
+
+#paths for depth data
+depths1_path = "/Users/colemandavidkane/Documents/BSU/GEOPH_522/homework/GMdepth1.txt"
+depths2_path = "/Users/colemandavidkane/Documents/BSU/GEOPH_522/homework/GMdepth2.txt"
+
+#import depth data into nparrays
+depths1 = np.loadtxt(depths1_path)
+depths2 = np.loadtxt(depths2_path)
+
+#calculates arithmetic mean for the two transects
+mean1 = np.mean(depths1)
+mean2 = np.mean(depths2)
+
+#calculates median for the two transects
+med1 = np.median(depths1)
+med2 = np.median(depths2)
+
+#calculates mode for two transects
+#st.mode returns value and count, '[0]' added to just get value
+mode1 = st.mode(depths1)[0]
+mode2 = st.mode(depths2)[0]
+
+#func for calculating iqr by finding difference between q3 and q1 for a dataset
+def iqr_calc(data):
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+    iqr = q3 - q1
+    return iqr
+
+#assigns value returned from iqr_calc to variables
+iqr1 = iqr_calc(depths1)
+iqr2 = iqr_calc(depths2)
+
+
