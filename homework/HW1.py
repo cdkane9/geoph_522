328f2011d1ae098cae3f24db744c94700ae628ff
cdkane9 <colemankane@boisestate.edu>
2024-09-05 21:39:23 -0600

    plotted pdf and rdh on one graph, made a mess of finding cdf

--- homework/HW1.py
+++ homework/HW1.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import scipy
 from scipy import stats as st
 import matplotlib.pyplot as plt
+import sympy as smp
 
 #paths for depth data
 depths1_path = "/Users/colemandavidkane/Documents/BSU/GEOPH_522/homework/GMdepth1.txt"
@@ -35,6 +37,14 @@ def stat_table(caca):
     ]
     df = pd.DataFrame({"stats": stats, "values": vals})
     return df
+
+
+#prints out the two stat tables w/ crude title
+print(f"GMdepth1 \n {stat_table(depths1)}")
+print()
+print(f"GMdepth2 \n {stat_table(depths2)}")
+print()
+
 
 ######################################################################
 #The following code creates a histogram and boxplot for each depth transect (depths1 and depths2)
@@ -79,7 +89,7 @@ hist2 = plt.hist(depths2, 30)
 plt.xlim(50,140)
 plt.ylim(0,6)
 
-plt.show()
+#plt.show()
 
 ######################################################################
 
@@ -87,45 +97,39 @@ plt.show()
 #define a function that will output pdf and rdh.  easier this way as will only
 #   write code once.  'dataset' and 'width' passed as arguments to make it easier to
 #   play around with histogram binwidth or individual datasets
-def pdf_calc(data, width):
-
-    #calculate mean for dataset
-    mu = np.mean(data)
-
-    ######################################
-
-
+def rdh(caca, width = 10):
 
     #np.histogram returns two arrays, one for nc (number of counts) and one for xvals of bins
-    counts, xbins = np.histogram(data, bins = width)
+    counts, xbins = np.histogram(caca, bins = width)
 
     #calculate the difference between bin limits
-    dx = xbins[2] - xbins[1]
+    dx = xbins[1] - xbins[0]
 
     #calculate the area of each bin
     area = counts * dx
 
-    #divbdes each count by the sum total area of histogram
+    #divides each count by the sum total area of histogram
     #outputs array of normalized bin heights
     rdh = counts / sum(area)
 
-    #######################
-    #create rel. density histogram
-    #plt.bar(x coords of bars, heights, bin_width)
-    #xbins [:-1] + dx / 2 sets the x coordinates to be in the middle of the bin
+    # plt.bar(x coords of bars, heights, bin_width)
+    # xbins [:-1] + dx / 2 sets the x coordinates to be in the middle of the bin
     #   i.e. so if the bins are 10-20, 20-30, 30-40, dx = 10, start at first xbin and add 5 (dx/2)
     #   then xcoords will be 15, 25, 35
 
-    plt.bar(xbins[:-1] + dx / 2, rdh, width = dx, edgecolor = 'black')
+    rdh = plt.bar(xbins[:-1] + dx / 2, rdh, width=dx, edgecolor='black')
 
+    return rdh
 
-
-
+def pdf(caca):
     #############################
     #create probability density function
 
+    # calculate mean for dataset
+    mu = np.mean(caca)
+
     #calcuate std.  ddof ==> delta degrees of freedom.  unsure
-    std = np.std(data, ddof = 1)
+    std = np.std(caca, ddof = 1)
 
     #np.linspace returns evenly space numbers over interval
     #   np.linspace(start, stop, number of samples to generate)
@@ -146,29 +150,68 @@ def pdf_calc(data, width):
     #putting together a,b,c for pdf
     f = a * np.exp(b/c)
 
+    return plt.plot(x, f, 'r', linewidth = 3), plt.xlim([.9 * min(x), 1.1 * max(x)]), plt.ylim([0,0.04])
 
-    #plt.pot(x-vals, y-vals, color, linewidth)
-    plt.plot(x, f, 'r', linewidth = 3)
+plt.subplot(1,2,1)
+rdh(depths1)
+pdf(depths1)
 
-    #sets max/min for domain and range
-    plt.xlim([mu - 3 * std, mu + 3 * std])
-    plt.ylim([0, 1.1 * max(f)])
+plt.subplot(1,2,2)
+rdh(depths2, 12)
+pdf(depths2)
+plt.tight_layout()
 
-    plt.show()
-
-
-#produce histogram and pdf for both datasets
-pdf_calc(depths2, 10)
-pdf_calc(depths1, 12)
+#plt.show()
 
 
 
 
-#prints out the two stat tables w/ crude title
-print(f"GMdepth1 \n {stat_table(depths1)}")
-print()
-print(f"GMdepth2 \n {stat_table(depths2)}")
-print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def f(caca):
+    #function for calculating cumulative density function
+    #inputs: dataset, lowerlimit, upper limit
+    #outputs:
+    #steps: bring in dataset, find pdf, integrate pdf to get cdf
+    #code for finding pdf is the same as above
+    mu = np.mean(caca)
+    std = np.std(caca, ddof=1)
+    x = smp.symbols('x', real = True)
+    a = 1 / (std * np.sqrt(2 * np.pi))
+    b = -(x - mu) ** 2
+    c = 2 * std ** 2
+    pdf = a * np.exp(b / c)
+    return pdf
+
+print(f(depths1))
+
+def cdf(pdf,low, high, caca):
+    #now integrate pdf to find cdf
+    cdf = scipy.integrate.quad(pdf, low, high, args = caca)
+
+    return cdf
+
+
+
+
+
+
 
 
 #answering questions at bottom of HW document
